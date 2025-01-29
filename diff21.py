52f4dc70f73ec405932fefbbc09131b81c8bae4b
Aditya Ashok Naik <muddu1403@gmail.com>
2025-01-29 22:11:06 +0530

    diff21
    Program that returns absolute difference of the input integer in case of n<21
    else returns twice the absolute difference.

--- diff21.py
+++ diff21.py
@@ -4,3 +4,20 @@
 # diff21(19) → 2
 # diff21(10) → 11
 # diff21(21) → 0
+
+
+#we proceed by defining a function diff21(n)
+
+def diff21(n):
+    if n > 21:
+        return abs(n-21) * 2
+    
+    else:
+
+        return abs(n-21)
+    
+if __name__ == "__main__" :
+
+    print(diff21(21))
+#all test cases are running as expected👍
+

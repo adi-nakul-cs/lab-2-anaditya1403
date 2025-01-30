01dc5e435554f1fd4dbd8ec8ef5643650d9e86ac
Aditya Ashok Naik <muddu1403@gmail.com>
2025-01-30 13:40:48 +0530

    misssing_char

--- missing_char.py
+++ missing_char.py
@@ -1,6 +1,25 @@
-# Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+# Given a non-empty string and an int n, return a new string where the char at index n has been removed. 
+# The value of n will be a valid index of a char in the original string 
+# (i.e. n will be in the range 0..len(str)-1 inclusive).
 
 
 # missing_char('kitten', 1) → 'ktten'
 # missing_char('kitten', 0) → 'itten'
 # missing_char('kitten', 4) → 'kittn'
+
+def missing_char(str,n):
+    if len(str) >= 1:
+        front = str[ :n]
+
+        rear = str[n + 1:]
+
+        return front + rear 
+    else:
+        print("String is empty.")
+
+if __name__ == "__main__":
+    print(missing_char("Anaditya",1))
+
+    print(missing_char("Weekend",4))
+
+    print(missing_char("abcde f",5))

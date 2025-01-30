bb8c9aabe8d7613be2ba347f2705d94b88cb4c2a
Aditya Ashok Naik <muddu1403@gmail.com>
2025-01-30 11:29:42 +0530

    front_back(str)
    Changing the first and last letters of the string i.e [-1] & [0].
    iff len(str) >=1

--- front_back.py
+++ front_back.py
@@ -5,3 +5,20 @@
 # front_back('a') → 'a'
 # front_back('ab') → 'ba'
 
+def front_back(str):
+    if len(str) <= 1:
+        return str
+    else:
+        return str[-1] + str[1:-1] + str[0]
+
+if __name__ == "__main__":
+    print(front_back("Adity"))
+
+    print(front_back("Zebra"))
+
+    print(front_back("Xenon"))
+
+    print(front_back("A"))
+
+    print(front_back("ab"))
+#All test cases are running as expected

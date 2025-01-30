60dc496b6080c56ab2e2a303a0fc5e3e5d3d4639
Aditya Ashok Naik <muddu1403@gmail.com>
2025-01-30 11:43:26 +0530

    makes10
    description included in program.👍

--- makes10.py
+++ makes10.py
@@ -4,3 +4,32 @@
 # makes10(9, 10) → True
 # makes10(9, 9) → False
 # makes10(1, 9) → True
+
+
+#we define a function including 2 arguments a and b.
+# so if a=10 *or* b=10 *or* a + b = 10
+# it returns True
+# else False
+
+
+def makes10(a,b):
+    if a == 10 or b == 10 or a + b == 10:
+        return True
+    
+    else:
+        return False
+    
+if __name__ == "__main__":
+    print(makes10(4,6))
+
+    print(makes10(10,20))
+
+    print(makes10(36,10))
+
+    print(makes10(-20,30))
+
+    print(makes10(6,3))
+
+    print(makes10(40,-30))
+
+    print(makes10(9,0))

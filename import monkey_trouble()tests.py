0190e9542fa078bd94588e9ba4a7f1e0c34729f2
Aditya Ashok Naik <muddu1403@gmail.com>
2025-02-01 15:12:57 +0530

    monkey_trouble

---
+++ import monkey_trouble()tests.py
@@ -0,0 +1,10 @@
+from monkey_trouble import monkey_trouble
+print(monkey_trouble(True,True))
+
+print(monkey_trouble(True,False))
+
+print(monkey_trouble(False,False))
+
+print(monkey_trouble(False,True))
+
+
--- monkey_trouble.py
+++ monkey_trouble.py
@@ -1,6 +1,26 @@
-# We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+# We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. 
+# We are in trouble if they are both smiling or if neither of them is smiling.
+#  Return True if we are in trouble.
 
 
 # monkey_trouble(True, True) → True
 # monkey_trouble(False, False) → True
 # monkey_trouble(True, False) → False
+
+# Function to determine if we are in trouble based on the monkeys' smiles
+def monkey_trouble(a_smile,b_smile):
+    if a_smile and b_smile:
+        return True
+
+    if not a_smile and not b_smile:
+        return True
+
+    else:
+        return False
+
+if __name__ == '__main__':
+    print("Are the two monkeys staring at us?")
+
+    print(monkey_trouble(False,False))
+
+

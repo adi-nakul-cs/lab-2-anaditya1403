5f49358b06a518984ae012e70b681fa1cb376d4b
Aditya Ashok Naik <muddu1403@gmail.com>
2025-02-01 21:55:24 +0530

    near hundred

--- near_hundred.py
+++ near_hundred.py
@@ -1,6 +1,17 @@
-# Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+# Given an int n, return True if it is within 10 of 100 or 200. 
+# Note: abs(num) computes the absolute value of a number.
 
 
 # near_hundred(93) → True
 # near_hundred(90) → True
 # near_hundred(89) → False
+
+def near_hundred(n):
+    return (abs(100 - n) <= 10) or (abs(200 - n) <= 10)
+
+# Test cases
+print(near_hundred(93))  # True
+print(near_hundred(90))  # True
+print(near_hundred(89))  # False
+print(near_hundred(99))  # True
+print(near_hundred(190)) # True
